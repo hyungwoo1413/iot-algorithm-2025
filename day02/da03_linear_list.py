71ba825a360647364445e71bbf55649a1c1d0fc6
hyungwoo1413 <glhwguddn@gmail.com>
2025-02-17 17:15:16 +0900

    2025-02-17

--- day02/da03_linear_list.py
+++ day02/da03_linear_list.py
@@ -1,16 +1,18 @@
 # 선형리스트 일반 구현
-# 실제 파이썬이 이렇게 동작하는 것이 아님!
+# 실제 파이썬이 이렇게 동작하는 게 아님
 # 배열(선형리스트)이 어떻게 동작하는지 로직을 파악할 것
-katok = [] # 빈 배열
-select = -1 # -1:통과 1:추가 2:삽입 3:삭제
+## 전역변수 선언 부분
+katok = []  # 빈 배열
+select = -1  # -1:통과, 1:추가, 2:삽입, 3:삭제, 4:종료
 
-# 데이터 추가
+## 함수 선언 부분
+# 데이터 추가.
 def add_data(friend):
     katok.append(None)
     lenKatok = len(katok)
     katok[lenKatok - 1] = friend
 
-# 중간 데이터 삽입
+# 중간 데이터 삽입.
 def insert_data(pos, friend):
     # 잘못된 인덱스를 넣었을때 예외처리 필요
     if pos < 0 or pos > len(katok):
@@ -20,16 +22,20 @@ def insert_data(pos, friend):
     katok.append(None) # 빈칸 추가
     lenKatok = len(katok)
 
-    for i in range(lenKatok -1, pos, -1): # 추가할 위치까지 데이터를 뒤로 이동
+    for i in range(lenKatok - 1, pos, -1):  # 추가할 위치까지 데이터를 뒤로 이동
         katok[i] = katok[i - 1]
         katok[i - 1] = None
 
     katok[pos] = friend
 
-# 데이터 삭제
+# 데이터 삭제.
 def delete_data(pos):
+    if pos < 0 or pos > len(katok):
+        print('삭제 범위를 벗어났습니다.')
+        return
+    
     lenKatok = len(katok)
-    katok[pos] = None # 지정된 위치 값을 삭제
+    katok[pos] = None  # 지정된 위치 값을 삭제
 
     for i in range(pos+1, lenKatok):
         katok[i - 1] = katok[i]
@@ -39,7 +45,7 @@ def delete_data(pos):
 
 ## 메인코드 영역
 if __name__ == '__main__':
-    
+
     while True:
         select = int(input('선택(1:추가, 2:삽입, 3:삭제, 4:종료) > '))
 
